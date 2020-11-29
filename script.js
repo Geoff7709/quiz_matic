f7928d70e4ffac549b3dd30e5f65d26fa127a274
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-11-29 16:43:33 -0600

    Beginning html/css with pseudocoding for js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create Quiz-Matic game
+// Declare variables for questions/answers
+// Set up timer set at 60 seconds for quiz
+// Set up score display
+// Question display block will have a simple statement of rules until start
+// Start play includes start timer and display first question - will be randomly selected from bank of twenty questions plus four bonus questions
+// Player selects answer with click function - if correct add to score. Each question will be worth ten points. Else answer is wrong, time is subtracted from amount.
+// Conditions for bonus - can pass and only lose time spent - if correct you gain twenty points plus fifteen seconds on the clock. Else double time is removed from timer.
+// Timer reaches zero game ends, player is asked for initials to save score to "High Scores" list
